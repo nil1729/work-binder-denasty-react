d7d026f443e359770cfe502a47521d9b584cc000
nil1729 <nilanjan172nsvian@gmail.com>
2021-09-26 23:44:30 +0530

    new API

--- controllers/dynasty-native/list_players.js
+++ controllers/dynasty-native/list_players.js
@@ -6,6 +6,7 @@ const _ = require('lodash');
 const DEFAULT_LIMIT = Number(process.env.PAGE_LIMIT);
 const DEFAULT_PAGE = Number(process.env.PAGE_START);
 const { google } = require('googleapis');
+const uuid = require('uuid');
 
 exports.getPlayerList = asyncHandler(async (req, res, next) => {
 	if (!['STANDARD', 'SF-TE-PREMIUM'].includes(req.params.league_format))
@@ -49,5 +50,39 @@ exports.getPlayerList = asyncHandler(async (req, res, next) => {
 			});
 		});
 		return res.status(200).json(playersRanks);
+	} else throw new ErrorResponse(`No players found`, 404);
+});
+
+exports.getAllPlayers = asyncHandler(async (req, res, next) => {
+	if (!req.GOOGLE_OAUTH_CREDENTIALS)
+		throw new ErrorResponse(`Error while retrieving data from source`, 500);
+	const sheets = google.sheets({ version: 'v4', auth: req.GOOGLE_OAUTH_CREDENTIALS });
+
+	const resp = await sheets.spreadsheets.values.get({
+		spreadsheetId: process.env.SPREADSHEET_ID,
+		range: `PPR!A2:F301`,
+	});
+
+	if (resp.data.values) {
+		const playerList = [];
+		resp.data.values.forEach((player) => {
+			playerList.push({
+				name: player[1],
+				player_id: uuid.v4(),
+			});
+		});
+
+		const part_one = playerList.sort(() => Math.random() - 0.5);
+		const part_two = playerList.sort(() => Math.random() - 0.5);
+		const part_three = playerList.sort(() => Math.random() - 0.5);
+		const whole_question = part_one.concat(part_two, part_three);
+
+		return res.status(200).json({
+			metadata: {
+				total_questions: whole_question.length / 6,
+				total_players: whole_question.length / 3,
+			},
+			data: whole_question,
+		});
 	} else throw new ErrorResponse(`No players found`, 404);
 });
--- routes/dynasty-native/list_players.js
+++ routes/dynasty-native/list_players.js
@@ -2,11 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 // Import Controllers
-const { getPlayerList } = require('../../controllers/dynasty-native/list_players');
+const { getPlayerList, getAllPlayers } = require('../../controllers/dynasty-native/list_players');
 
 // Google Sheets Config
 const { authorizeQuery } = require('../../config/sheets_authorize');
 
 router.route('/:league_format').get(authorizeQuery, getPlayerList);
+router.route('/get_players/all').get(authorizeQuery, getAllPlayers);
 
 module.exports = router;
