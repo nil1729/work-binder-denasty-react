64006cda625a74b16613c1d50d9622a115ec6dc3
sravankumar2 <sravankumar02031996@gmail.com>
2021-10-05 08:57:11 +0530

    changes on users

--- frontend/src/components/Sidebar/index.js
+++ frontend/src/components/Sidebar/index.js
@@ -1,52 +1,77 @@
-import React, { useState } from 'react';
-import './sidebar.css';
+import React, { useState } from "react";
+
 import {
-	SidebarContainer,
-	Icon,
-	CloseIcon,
-	SidebarWrapper,
-	SidebarLink,
-	SidebarMenu,
-} from './SidebarElements';
+  SidebarContainer,
+  Icon,
+  CloseIcon,
+  SidebarWrapper,
+  SidebarLink,
+  SidebarMenu,
+} from "./SidebarElements";
 
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
+import styles from "./sidebar.module.scss";
 const Sidebar = ({ isOpen, toggle }) => {
-	const sideMenus = [
-		{
-			title: 'home',
-			path: '/',
-		},
-		{
-			title: 'blog',
-			path: '/blog',
-		},
-		{
-			title: 'trade calculator',
-			path: '/trade_calculator',
-		},
-		{
-			title: 'rankings',
-			path: '/rankings',
-		},
-		{
-			title: 'login',
-			path: '/login',
-		},
-	];
-	return (
-		<nav className={isOpen ? 'nav-menu active' : 'nav-menu'}>
-			<ul className='nav-menu-items' onClick={toggle}>
-				{sideMenus.map((item, index) => {
-					return (
-						<li key={index} className='nav-text'>
-							<Link to={item.path}>{item.title}</Link>
-						</li>
-					);
-				})}
-			</ul>
-		</nav>
-	);
+  const sideMenus = [
+    {
+      title: "home",
+      path: "/",
+    },
+    {
+      title: "blog",
+      path: "/blog",
+    },
+    {
+      title: "trade calculator",
+      path: "/trade_calculator",
+    },
+    {
+      title: "rankings",
+      path: "/rankings",
+    },
+    {
+      title: "login",
+      path: "/login",
+    },
+  ];
+  const [leagueFormat, setLeagueFormat] = useState("ON");
+  const changeTab = (tab) => async () => {
+    setLeagueFormat(tab);
+  };
+  return (
+    <nav className={`${styles.nav_menu} ${isOpen ? styles.active : ""}`}>
+      <ul className={styles.nav_menu_items} onClick={toggle}>
+        {sideMenus.map((item, index) => {
+          return (
+            <li key={index} className={styles.nav_text}>
+              <Link to={item.path}>{item.title}</Link>
+            </li>
+          );
+        })}
+        <li className={styles.nav_text}>
+          <div className={styles.tab__btn__container}>
+            <div
+              className={`${styles.tab__btn} ${
+                leagueFormat === "ON" ? styles.tab__active : ""
+              }`}
+              onClick={changeTab("ON")}
+            >
+              ON
+            </div>
+            <div
+              className={`${styles.tab__btn} ${
+                leagueFormat === "OFF" ? styles.tab__active : ""
+              }`}
+              onClick={changeTab("OFF")}
+            >
+              OFF
+            </div>
+          </div>
+          <p>Dark Mode</p>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Sidebar;
--- frontend/src/pages/PPR.js
+++ frontend/src/pages/PPR.js
@@ -1,102 +1,115 @@
-import React, { useState, useEffect } from 'react';
-import styles from '../components/PPR/index.module.scss';
-import { connect } from 'react-redux';
-import { getRankings } from '../store/actions/dynasty/rankings';
-import LinearProgress from '@material-ui/core/LinearProgress';
-import Pagination from '@material-ui/lab/Pagination';
-
+import React, { useState, useEffect } from "react";
+import styles from "../components/PPR/index.module.scss";
+import { connect } from "react-redux";
+import { getRankings } from "../store/actions/dynasty/rankings";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import InfiniteScroll from "react-infinite-scroll-component";
 function PPR({ getRankings }) {
-	const [selectedTab, setSelectedTab] = useState(1);
-	const [fetchingList, setFetchingList] = useState(true);
-	const [rankingList, setRankingList] = useState([]);
-	const [currentRankingType, setCurrentRankingType] = useState('PPR');
-	const [myPagination, setMyPagination] = useState({ page: 1 });
+  const [leagFormat, setLeagueFormat] = useState("PPR");
+  const [rankingList, setRankingList] = useState([]);
+  const [hasMore, setHasMore] = useState(true);
+  const [currentPage, setCurrentPage] = useState(0);
 
-	useEffect(() => {
-		fetchData();
-		// eslint-disable-next-line
-	}, []);
+  useEffect(
+    (leagFormat) => {
+      fetchMoreData(leagFormat);
+      // eslint-disable-next-line
+    },
+    [leagFormat]
+  );
 
-	const changeTab = (tabIndex) => async () => {
-		if (selectedTab === tabIndex) return;
-		const rankType = tabIndex === 1 ? 'PPR' : 'SF-TE-PREMIUM';
-		setSelectedTab(tabIndex);
-		setCurrentRankingType(rankType);
-		setMyPagination({ page: 1 });
-		fetchData(rankType);
-	};
+  const changeTab = (tabIndex) => async () => {
+    setLeagueFormat(tabIndex);
 
-	const pageChange = (event, value) => {
-		setMyPagination({ page: value });
-		fetchData(currentRankingType, value);
-	};
+    setRankingList([]);
+    setCurrentPage(0);
+  };
 
-	const fetchData = async (rankType = 'PPR', pageNum = 1) => {
-		setFetchingList(true);
-		const dt = await getRankings(rankType, pageNum);
-		setRankingList(dt);
-		setFetchingList(false);
-	};
+  const fetchMoreData = async (page = currentPage + 1) => {
+    try {
+      const res = await getRankings(leagFormat, page);
+      console.log(res);
+      setCurrentPage(currentPage + 1);
+      setRankingList([...rankingList, ...res]);
+    } catch (e) {
+      console.log(e);
+      setHasMore(false);
+    }
+  };
 
-	return (
-		<div className={styles.container}>
-			<h2 className={styles.page_title}>PPR Rankings</h2>
-			<div className={styles.tab_btn_container}>
-				<div
-					className={`${styles.tab_btn} ${selectedTab === 1 ? styles.tab_active : ''}`}
-					onClick={changeTab(1)}
-				>
-					PPR
-				</div>
-				<div
-					className={`${styles.tab_btn} ${selectedTab === 2 ? styles.tab_active : ''}`}
-					onClick={changeTab(2)}
-				>
-					SuperFlex | TE Premium
-				</div>
-			</div>
-			<div className={styles.table}>
-				<div className={styles.table__header}>
-					<div className={styles.header__item}>rank</div>
-					<div className={`${styles.header__item} ${styles.name__column}`}>name</div>
-					<div className={styles.header__item}>team</div>
-					<div className={styles.header__item}>pos</div>
-				</div>
-				<div className={styles.table__content}>
-					{fetchingList ? (
-						<LinearProgress style={{ marginTop: '1rem' }} />
-					) : (
-						<>
-							{rankingList.map((player, index) => (
-								<div
-									className={`${(index + 1) % 2 === 0 ? styles.odd_row : ''} ${styles.table__row}`}
-								>
-									<div className={`${styles.table__data} ${styles.rank_index}`}>
-										<h3>{player.rank}</h3>
-									</div>
-									<div className={`${styles.table__data} ${styles.name__column}`}>
-										{player.name}
-									</div>
-									<div className={`${styles.uppercase} ${styles.table__data}`}>{player.team}</div>
-									<div className={`${styles.uppercase} ${styles.table__data}`}>
-										{player.position}
-									</div>
-								</div>
-							))}
-							<div className={styles.pagination_container}>
-								<Pagination
-									count={10}
-									color='primary'
-									onChange={pageChange}
-									page={myPagination.page}
-								/>
-							</div>
-						</>
-					)}
-				</div>
-			</div>
-		</div>
-	);
+  return (
+    <div className={styles.container}>
+      <h2 className={styles.page_title}>PPR Rankings</h2>
+      <div className={styles.tab_btn_container}>
+        <div
+          className={`${styles.tab_btn} ${
+            leagFormat === "PPR" ? styles.tab_active : ""
+          }`}
+          onClick={changeTab("PPR")}
+        >
+          PPR
+        </div>
+        <div
+          className={`${styles.tab_btn} ${
+            leagFormat === "SF-TE-PREMIUM" ? styles.tab_active : ""
+          }`}
+          onClick={changeTab("SF-TE-PREMIUM")}
+        >
+          SuperFlex | TE Premium
+        </div>
+      </div>
+      <div className={styles.table}>
+        <div className={styles.table__header}>
+          <div className={styles.header__item}>rank</div>
+          <div className={`${styles.header__item} ${styles.name__column}`}>
+            name
+          </div>
+          <div className={styles.header__item}>team</div>
+          <div className={styles.header__item}>pos</div>
+        </div>
+        <div className={styles.table__content}>
+          {rankingList.length >= 0 ? (
+            <InfiniteScroll
+              dataLength={rankingList.length}
+              next={fetchMoreData}
+              hasMore={hasMore}
+              loader={<LinearProgress />}
+              endMessage={
+                <p style={{ textAlign: "center" }}>
+                  <b>Yay! You have seen it all</b>
+                </p>
+              }
+            >
+              {rankingList.map((player, index) => (
+                <div
+                  className={`${(index + 1) % 2 === 0 ? styles.odd_row : ""} ${
+                    styles.table__row
+                  }`}
+                >
+                  <div className={`${styles.table__data} ${styles.rank_index}`}>
+                    <h3>{player.rank}</h3>
+                  </div>
+                  <div
+                    className={`${styles.table__data} ${styles.name__column}`}
+                  >
+                    {player.name}
+                  </div>
+                  <div className={`${styles.uppercase} ${styles.table__data}`}>
+                    {player.team}
+                  </div>
+                  <div className={`${styles.uppercase} ${styles.table__data}`}>
+                    {player.position}
+                  </div>
+                </div>
+              ))}
+            </InfiniteScroll>
+          ) : (
+            <LinearProgress />
+          )}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default connect(null, { getRankings })(PPR);
